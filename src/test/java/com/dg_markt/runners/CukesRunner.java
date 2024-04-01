ccf57b68401e117ff71fa9361c83f4e11d34e725
SukruYuksel <yksl3743dc@gmail.com>
2024-04-01 13:22:20 +0200

    json report düzenlendic

--- src/test/java/com/dg_markt/runners/CukesRunner.java
+++ src/test/java/com/dg_markt/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"json:target/cucumber/cucumber.json",
+        plugin = {"json:target/cucumber.json",
                 "html:target/default-html-report.html",
                 "rerun:target/rerun.txt",
         },
